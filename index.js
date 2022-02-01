7c743e32740f5f73be8c010bffd66e1c04204e44
Wes Bourke <wesbourkeavl@gmail.com>
2022-02-01 13:38:15 -0600

    commit tree 37aa8fbbf8216efbd2b269c09b0cd32854626e6c

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(a) {
+    a++;
+    return a;
+}
+
+function decrement(a) {
+    a--;
+    return a;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
